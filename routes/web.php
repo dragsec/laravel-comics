c3c4a21cade6eb80d0115564bdd13561b73edd19
dragsec <draganyes@gmail.com>
2021-06-24 14:56:00 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $comicsInfo = config('comics');
 
-    return view('home');
-});
+    $comicsArray = [
+        "comicsList" => $comicsInfo
+    ];
+
+    return view('homepage');
+})->name('homepage');
 
 
 
+
+Route::get('/placeholder', function() {
+    return "Page not ready";
+})->name('placeholder');
