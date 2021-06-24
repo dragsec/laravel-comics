2c269a3e9e18aa6cc8e77c822f3e978320c50983
dragsec <draganyes@gmail.com>
2021-06-24 16:59:54 +0200

    header amatoriale

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layout')
+@extends('layouts.default')
 
 @section('titolo', 'homepage')
 
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+<header>
+    <div class="header1 BlueWhite">
+        <div class="container">
+            <div class="header1font">
+                <span>DC POWER VISA&reg;</span>
+                <span>ADDITIONAL DC SITES <i class="fa fa-sort-desc"></i></span>
+            </div>
+        </div>
+    </div>
+
+    <div class="header2 container">
+        <ul class="header2UL"> 
+            <li><img src="{{ asset('images/dc-logo.png') }}"  alt="" height="60px" width="60px"></li>
+            <li>CHARACTERS</li>
+            <li class="blueunderline">COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+            <li><a class="blueunderline padleft40px">Search <i class="fa fa-search"></i></a></li>
+        </ul>
+
+    </div>
+
+    <img src="{{ asset('images/jumbotron.jpg') }}" class="jumbo" alt="">
+
+</header>
