df109b1f302e3826c77a1e432784ffbe4339ed5e
dragsec <draganyes@gmail.com>
2021-06-25 04:44:49 +0200

    manca single

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,77 @@
+<footer>
+    <div class="footer1" style="background-image: url('{{asset('images/footer-bg.jpg')}}')">
+        <div class="container flex">
+            <div>
+                <div class="footerDiv1">
+                    <div class="footerNav">
+                        <span class="fontbig white">DC COMICS</span>
+                        <ul class="footer2UL">
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Tv</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <span class="fontbig white">SHOP</span>
+                        <ul class="footer2UL">
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="footerNav">
+                        <span class="fontbig white">DC</span>
+                        <ul class="footer2UL">
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="footerNav">
+                        <span class="fontbig white">SITES</span>
+                        <ul class="footer2UL">
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="fontgray footerNav">
+                    <p>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#" class="bluefont">noted here</a>. All rights reserved.</p>
+                    <p><a href="#" class="bluefont">Cookies Settings</a></p>
+                </div>
+            </div>
+            <div class="dcPic">
+                <img src="{{ asset('images/dc-logo-bg.png') }}" height="400px">
+            </div>
+        </div>
+    </div>
+
+
+    <div class="footer2">
+        <div class="container alignCenter">
+            <div class="btnBorder white">SIGN-UP NOW!</div>
+            <div class="row">
+                <a class="bluefont fontbig follow">FOLLOW US</a>
+                <ul class="footer2UL ultest">
+                    <li><a href="#"><img src="{{ asset('images/footer-facebook.png') }}"></a></li>
+                    <li><a href="#"><img src="{{ asset('images/footer-twitter.png') }}"></a></li>
+                    <li><a href="#"><img src="{{ asset('images/footer-youtube.png') }}"></a></li>
+                    <li><a href="#"><img src="{{ asset('images/footer-pinterest.png') }}"></a></li>
+                    <li><a href="#"><img src="{{ asset('images/footer-periscope.png') }}" ></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
